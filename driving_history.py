5c89a6203b9961357efe1dae164d085c4869eedc
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-05-17 23:10:22 -0400

    Aggregated all columns

--- driving_history.py
+++ driving_history.py
@@ -15,9 +15,15 @@ daily_resample = df.resample("D", on="Start Time").agg(
         "Total Distance (mi)": sum,
         "Top Speed (mph)": max,
         "Trip Time": sum,
-        "Idle Time": sum
+        "Idle Time": sum,
+        "Average Speed (mph)": pd.DataFrame.mean,
+        "Average mpg": pd.DataFrame.mean,
+        "Fuel Used (gal)": sum
     }
 )
+
+pd.set_option('display.max_columns', None)
+print(daily_resample.head())
 
 # Plot
 plt.show()

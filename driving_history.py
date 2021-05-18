eb5c651a614c96cf588869b39bf911b288b9b173
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-05-18 16:38:13 -0400

    Polished up code for article

--- driving_history.py
+++ driving_history.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Load
-df = pd.read_csv("data/2020-driving-history.csv")
+df = pd.read_csv("data/2019-2021-driving-history.csv")
 
 # Clean
 df["Start Time"] = pd.to_datetime(df["Start Time"])
+df["End Time"] = pd.to_datetime(df["End Time"])
 df["Trip Time"] = pd.to_timedelta(df["Trip Time"])
 df["Idle Time"] = pd.to_timedelta(df["Idle Time"])
 
@@ -25,9 +26,12 @@ daily_resample = df.resample("D", on="Start Time").agg(
 daily_resample["Trip Time"] = daily_resample["Trip Time"].dt.total_seconds()
 daily_resample["Idle Time"] = daily_resample["Idle Time"].dt.total_seconds()
 
+cumulative_daily_resample = daily_resample.cumsum()
+
 # Plot
 daily_resample.plot.area(subplots=True)
 daily_resample.plot.area(y=["Idle Time", "Trip Time"])
+cumulative_daily_resample.plot.area(y=["Total Distance (mi)", "Fuel Used (gal)"], subplots=True)
 
 plt.tight_layout()
 plt.show()
